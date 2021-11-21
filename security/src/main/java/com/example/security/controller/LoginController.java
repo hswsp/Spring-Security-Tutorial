1e61bff872f3286fa3b989d246767c5f1e5b7988
hswsp <hswsp@mail.ustc.edu.cn>
2021-11-21 11:52:43 +0800

    tutorial 1

--- security/src/main/java/com/example/security/controller/LoginController.java
+++ security/src/main/java/com/example/security/controller/LoginController.java
@@ -8,10 +8,7 @@ import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
